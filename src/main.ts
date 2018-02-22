05ee7b3a1a6e09912dff00c8239f548404563cd6
Daniel Djordjevic <daniel.djordjevic@outlook.fr>
2018-02-22 14:41:16 +0100

    Test commit for webhook
    Descripition test commit webhook

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// add comment hammer test
 import 'hammerjs';
 
 if (environment.production) {
