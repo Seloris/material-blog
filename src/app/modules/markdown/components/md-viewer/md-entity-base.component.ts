aeee455e59f078190ac0c084267ba299d234c3fc
Daniel Djordjevic <daniel.djordjevic@viacesi.fr>
2018-02-28 19:19:27 +0100

    ADD regex + replaces WIP

--- src/app/modules/markdown/components/md-viewer/md-entity-base.component.ts
+++ src/app/modules/markdown/components/md-viewer/md-entity-base.component.ts
@@ -1,6 +1,50 @@
 import { Input } from '@angular/core';
-import { MdEntity, IMdEntity } from '@selo/markdown';
+import { IMdEntity, MdEntityTypeEnum } from '@selo/markdown';
+import { RegexConstants } from '../../../../../libs/regex.constants';
 
 export abstract class MdEntityBaseComponent<T extends IMdEntity> {
     @Input() entity: T;
+    get formattedContent() {
+        const content = this.entity.getCleanedContent();
+        if (this.entity.type !== MdEntityTypeEnum.Code) {
+            if (typeof (content) === 'string') {
+                return this.markdownToHtml(content);
+            } else {
+                return content.map(mdTxt => this.markdownToHtml);
+            }
+        }
+
+        return content;
+    }
+
+    private markdownToHtml(mdText: string): string {
+        mdText = this.markDownEmphasisToHtml(mdText);
+        return this.markDownStrike(mdText);
+
+    }
+
+    private getSpan(htmlClass: string, value: string) {
+        return `<span class="${htmlClass}">${value}</span>`;
+    }
+
+    private markDownEmphasisToHtml(mdText: string): string {
+        let match = RegexConstants.emphasis.exec(mdText);
+        while (match !== null) {
+            const emphasisClass = match[1].length === 1 ? 'italic' : 'bold';
+            mdText = mdText.replace(match[0], this.getSpan(emphasisClass, match[2]));
+            match = RegexConstants.emphasis.exec(mdText);
+        }
+
+        return mdText;
+    }
+
+    private markDownStrike(mdText: string): string {
+        let match = RegexConstants.strike.exec(mdText);
+        while (match !== null) {
+            mdText = mdText.replace(match[0], this.getSpan('strike', match[1]));
+            match = RegexConstants.strike.exec(mdText);
+        }
+
+        return mdText;
+    }
 }
---
+++ src/libs/regex.constants.ts
@@ -0,0 +1,4 @@
+export namespace RegexConstants {
+    export const emphasis = /([\*|_]+)(.*?)[\*|_]+/g;
+    export const strike = /~~(.*?)~~/g;
+}
