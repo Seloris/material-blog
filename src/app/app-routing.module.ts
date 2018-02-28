40b734f35b9b8138fcf58fdafe67cf7ffb444288
Unknown <daniel.djordjevic@viacesi.fr>
2018-02-28 16:15:09 +0100

    Make is hash nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SandboxPageComponent } from './components/sandbox-page/sandbox-page.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 const routes: Routes = [
     {
@@ -16,6 +17,11 @@ const routes: Routes = [
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
+    providers: [
+        {
+            provide: LocationStrategy,
+            useClass: HashLocationStrategy
+        }],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
