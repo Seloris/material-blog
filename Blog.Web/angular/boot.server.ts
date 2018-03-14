03776e5664a8381e0438852304b94f4f84d0e506
Daniel Djordjevic <daniel.djordjevic@outlook.fr>
2018-03-14 15:11:54 +0100

    WIP

---
+++ Blog.Web/angular/boot.server.ts
@@ -0,0 +1,5 @@
+import { enableProdMode } from '@angular/core';
+export { AppModule } from './app/app.module.server';
+export { MarkdownModule } from './app/modules/markdown/markdown.module';
+
+enableProdMode();
